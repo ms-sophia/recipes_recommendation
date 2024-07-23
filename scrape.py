4f4e2c62dd58905700244a1d8b28e2faccc7d204
ms-sophia <jardelezasophia@gmail.com>
2024-07-23 09:41:07 +0800

    changes made

--- scrape.py
+++ scrape.py
@@ -3,16 +3,79 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
+recipes = []
+urls = []
+ingredients = []
+string = []
+request_delay = 3
+urls_ingri = []
+pages_to_scrape = 100
+
+
+def scrape_ingri(url):
+    global string
+    try:
+        r = requests.get(url)
+        url_text = url.split("/")
+        print(f"recipe url:{r.status_code} {url_text[3]}")
+        # soup = BeautifulSoup(r.content, "lxml")
+        soup = BeautifulSoup(r.content, "html.parser")
+        s = soup.find("section", class_="oc-recipe-content")
+
+        # check for server errors or maintenance
+        if soup.title and "service unavailable" in soup.title.text.lower():
+            print(f"Server error on page {url} Skipping...")
+            print(f"deleting the name and url")
+            del recipes[-1]
+            del url[-1]
+            return 0
+
+        for ingredient in s.find_all("li", "wprm-recipe-ingredient"):
+            ingredient = ingredient.text
+            ingredient = ingredient.replace("▢ ", "")  # remove checkbox
+            ingredient = ingredient.replace("\n", "")
+            string.append(ingredient)
+        # print(string)
+        ingredients.append(string)
+        urls_ingri.append(url_text[3])
+        string = []
+        # sleep to add a delay between requests
+        time.sleep(request_delay)
+
+    except requests.exceptions.RequestException as e:
+        # Handle HTTP request errors (e.g connection issues)
+        print(f"Error on url {url}: {e}")
+        # delete url and names
+        print(f"deleting the name and url")
+        del recipes[-1]
+        del urls[-1]
+
+    except IndexError as e:
+        # Handle "list index out of range" error
+        print(f"Index error on url {url}: {e}")
+        # delete url and names
+        print(f"deleting the name and url")
+        del recipes[-1]
+        del urls[-1]
+
+    except Exception as e:
+        # Handle other unexpected errors
+        print(f"Unxpected error on url {url}: {e}")
+        # delete url and names
+        print(f"deleting the name and url")
+        del recipes[-1]
+        del urls[-1]
+
+    df = pd.DataFrame(data={"names": recipes, "url": urls, "ingredients": ingredients})
+    # df2 = pd.DataFrame(data={"ingredients": ingredients, "url": urls_ingri})
+    # df = df.explode("ingredients")
+    df.to_csv("data.csv", index=False)
+    # df2.to_csv("ingri_data.csv", index=False)
+
+    return 0
+
 
 def scrape():
-    recipes = []
-    urls = []
-    ingredients = []
-    string = []
-    request_delay = 3
-
-    pages_to_scrape = 100
-
     for page in range(1, pages_to_scrape + 1):
 
         try:
@@ -37,7 +100,10 @@ def scrape():
                 line = line.replace("\n", "")
                 print(line)
                 recipes.append(line)
-                urls.append(link.get("href"))
+                url = link.get("href")
+                urls.append(url)
+                scrape_ingri(url)
+
             # sleep to add a delay between requests
             time.sleep(request_delay)
 
@@ -53,48 +119,7 @@ def scrape():
             # Handle other unexpected errors
             print(f"Unxpected error on page {page}: {e}")
 
-    df1 = pd.DataFrame(data={"names": recipes, "url": urls})
-    df1.to_csv("names_url_data.csv", index=False)
-
-    for url in urls:
-        # url = "https://panlasangpinoy.com/kung-pao-chicken-recipe/"
-        try:
-            r = requests.get(url)
-            url_text = url.split("/")
-            print(f"recipe url:{r.status_code} {url_text[3]}")
-            # soup = BeautifulSoup(r.content, "lxml")
-            soup = BeautifulSoup(r.content, "html.parser")
-            s = soup.find("section", class_="oc-recipe-content")
-            # check for server errors or maintenance
-            if soup.title and "service unavailable" in soup.title.text.lower():
-                print(f"Server error on page {url} Skipping...")
-                continue
-
-            for ingredient in s.find_all("li", "wprm-recipe-ingredient"):
-                ingredient = ingredient.text
-                ingredient = ingredient.replace("▢ ", "")  # remove checkbox
-                ingredient = ingredient.replace("\n", "")
-                string.append(ingredient)
-            # print(string)
-            ingredients.append(string)
-            string = []
-            # sleep to add a delay between requests
-            time.sleep(request_delay)
-
-        except requests.exceptions.RequestException as e:
-            # Handle HTTP request errors (e.g connection issues)
-            print(f"Error on url {url}: {e}")
-        except IndexError as e:
-            # Handle "list index out of range" error
-            print(f"Index error on url {url}: {e}")
-        except Exception as e:
-            # Handle other unexpected errors
-            print(f"Unxpected error on url {url}: {e}")
-
-    # df = pd.DataFrame(data={"names": recipes, "url": urls, "ingredients": ingredients})
-    df2 = pd.DataFrame(data={"ingredients": ingredients})
-    # df = df.explode("ingredients")
-    # df.to_csv("data.csv", index=False)
-    df2.to_csv("ingri_data.csv", index=False)
-
-    return 0
+    # df1 = pd.DataFrame(data={"names": recipes, "url": urls})
+    # df1.to_csv("names_url_data.csv", index=False)
+    df1 = pd.DataFrame(data={"names": recipes, "url": urls, "ingredients": ingredients})
+    df1.to_csv("data.csv", index=False)
